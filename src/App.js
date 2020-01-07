03af2ccf0e27326cb5eff413c280f97f637fa1ae
Michael Jardine <michael.jardine@saongroup.com>
2020-01-07 09:52:19 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';  
 import './App.css';
+import Todos from './components/Todos';
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [ ]
+  }
+
+  componentDidMount() {
+    Axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then(res => this.setState( { todos : res.data }))
+  }
+
+  handleMarkComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    })  
+  });
+    console.log(id);
+  }
+
+  handleDeleteTodo = (id) => {
+    Axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    .then(res => this.setState( { todos : [...this.state.todos.filter(todo => todo.id !== id)] }))
+    console.log('derp');
+
+  }
+
+  handleAddTodo = (title) => {
+    const newId = (this.state.todos.length) + 1;
+    Axios.post('https://jsonplaceholder.typicode.com/todos', {
+      id: newId,
+      title: title,
+      completed: false
+    })
+      .then(res => this.setState( { todos : [...this.state.todos, res.data] }))
+      console.log(this.state.todos);
+  }
+
+    render() { 
+    return (
+      <div className="App">
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.handleAddTodo} />
+          <Todos  todos={this.state.todos} 
+                  markComplete={this.handleMarkComplete} 
+                  deleteTodo={this.handleDeleteTodo} />
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
